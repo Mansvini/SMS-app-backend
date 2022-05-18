717ff5dbf6cb27a7c3a9c712c5ef0f4beb17cba3
Mansvini <coderspecial@gmail.com>
2022-05-18 13:17:33 +0530

    updating

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const twilio = require('twilio');
 
 const app = express();
+app.use(cors());
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID; //change
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
-
-app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
-
 
 app.get('/', (req, res)=> {
   res.send('this.is working');
@@ -19,7 +15,6 @@ app.get('/', (req, res)=> {
 app.post('/SendMessage', (req, res)=>{
     client.messages
       .create({body: req.body.message, from: '+19472214483', to: req.body.phone})
-      .then(message => console.log(message.sid))
       .catch(err=>res.status(400).json('server error'));
 })
 
